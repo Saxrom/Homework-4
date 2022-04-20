5dfefc654096913906225165309f9ac08def56ad
Shahrom <shahromkr@code.edu.az>
2022-04-20 12:43:32 +0400

    Added Homework-4

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zehmet olmasa stringi daxil edin");
+
+            string str = Console.ReadLine();
+
+            Console.WriteLine(CapitalizeAllWords(str));
+        }
+
+        static  string CapitalizeAllWords(string str)
+        {
+            var sb = new StringBuilder(str.Length);
+            bool inWord = false;
+            foreach (var c in str)
+            {
+                if (char.IsLetter(c))
+                {
+                    sb.Append(inWord ? char.ToLower(c) : char.ToUpper(c));
+                    inWord = true;
+                }
+                else
+                {
+                    sb.Append(c);
+                    inWord = false;
+                }
+            }
+            return sb.ToString();
+
+
+        }
+    }
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Student student = new Student("Shahrom", "Rajabov", 324, 77, false);
+
+            Console.WriteLine(student.getInfo());
+        }
+
+    }
+
+    class Student
+    {
+        public string Name;
+        public string Surname;
+        public int Group;
+        public byte Point;
+        public bool IsGraduated;
+
+
+        public  Student(string name,string  surname,int group,byte point,bool isGraduated)
+        {
+            Name = name;
+            Surname = surname;
+            Group = group;
+            Point = point;
+
+            if (Point > 80)
+            {
+                Console.WriteLine("Telebe ikinci imtahana gire biler");
+            }
+
+            else
+            {
+                Console.WriteLine("Telebe ikinci imtahana gire bilmez");
+            }
+
+            IsGraduated = isGraduated;
+
+            if(isGraduated)
+            {
+                Console.WriteLine("Telebe mezun olub");
+            }
+
+            else
+            {
+                Console.WriteLine("Telebe mezun olmayib");
+            }
+
+
+        }
+
+
+        public string getInfo()
+        {
+       
+            return $"Name: {Name}  Surname:{Surname} Group:{Group} Point:{Point} isGrduate:{IsGraduated}";
+
+        }
+    }
+}
